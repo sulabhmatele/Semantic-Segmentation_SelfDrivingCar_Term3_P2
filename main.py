327e4e637436c395b36aaa50ecb1e544274e4238
Sulabh Matele <sulabhmatele@outlook.com>
2017-12-17 18:19:27 -0500

    Better results, after tuning some params on AWS

--- main.py
+++ main.py
@@ -56,24 +56,24 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     """
     # TODO: Implement function
     conv_1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding='same',
-                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-1))
     # Upsample - Deconvolution
     output1 = tf.layers.conv2d_transpose(conv_1, num_classes, 4, 2, padding='same',
-                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-1))
 
     conv_2 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, padding='same',
-                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-1))
     # Add skip layer
     skip1 = tf.add(conv_2, output1)
     output2 = tf.layers.conv2d_transpose(skip1, num_classes, 4, 2, padding='same',
-                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-1))
 
     conv_3 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, padding='same',
-                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-1))
 
     skip2 = tf.add(conv_3, output2)
     output = tf.layers.conv2d_transpose(skip2, num_classes, 16, 8, padding='same',
-                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-1))
     
     tf.Print(output, [tf.shape(output)[:]])
 
@@ -104,7 +104,7 @@ tests.test_optimize(optimize)
 
 
 def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
-             correct_label, keep_prob, learning_rate = 0.001):
+             correct_label, keep_prob, learning_rate):
     """
     Train neural network and print out the loss during training.
     :param sess: TF Session
@@ -127,11 +127,11 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     for i in range(epochs):
         for image, label in get_batches_fn(batch_size):
             _,loss = sess.run([train_op, cross_entropy_loss],
-                                feed_dict={input_image:image, correct_label:label, keep_prob:0.7, learning_rate:0.001})
-            total_accuracy += (loss * len(image))
+                                feed_dict={input_image:image, correct_label:label, keep_prob:0.5, learning_rate:(1e-2)})
+            # total_accuracy += (loss * len(image))
 
         print("EPOCH {} ...".format(i+1))
-        print("Training_loss = {:.3f}".format(total_accuracy))
+        print("Training_loss = {:.3f}".format(loss))
         print()
 
     print("Training Done...")
@@ -160,8 +160,8 @@ def run():
 
     with tf.Session() as sess:
         # Path to vgg model
-        num_epochs = 6
-        batch_size = 32
+        num_epochs = 20
+        batch_size = 8
         vgg_path = os.path.join(data_dir, 'vgg')
         # Create function to get batches
         get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
